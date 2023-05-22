3677b2c7ba53d9a375d8ba71ef1ab4aee53b42a9
carlosgd17 <carlosgd_17@outlook.com>
2023-05-22 17:22:25 -0600

    Definiendo la clase Transmisor

--- ModAndDemod/intento.py
+++ ModAndDemod/intento.py
@@ -1,1 +1,9 @@
-print("hola")
+class Transmisor:
+    def __init__(self):
+        self.bit_rate = 1000
+        self.frequency = 1500
+        self.volume = 2000
+
+    def transmit(self, msg):
+        # hace la magia
+        print(msg)
