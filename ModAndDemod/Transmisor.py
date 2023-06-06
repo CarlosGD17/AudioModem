6d0e6c365dd969b2e729af22e80b1d2d3ac336e6
CarlosGD17 <carlosgd_17@outlook.com>
2023-06-06 08:24:01 -0600

    Archivos

--- ModAndDemod/Transmisor.py
+++ ModAndDemod/Transmisor.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import random
+import bitarray
 
 
 class Baudio:
@@ -19,9 +20,14 @@ class Baudio:
     def transmit(self):
         p = pyaudio.PyAudio()
         # hace la magia
+        # x = [muestreo * duracion]
+        # sin(2*pi*x*frecuencia/muestreo)
         samples = (np.sin(
             2 * np.pi * np.arange(self.sampling_rate * self.duration) * self.frequency / self.sampling_rate)).astype(
             np.float32)
+
+        #plt.plot(self.volume * samples)
+        #plt.show()
 
         # per @yahweh comment explicitly convert to bytes sequence
         output_bytes = (self.volume * samples).tobytes()
@@ -35,7 +41,7 @@ class Baudio:
         # play. May repeat with different volume values (if done interactively)
         start_time = time.time()
         stream.write(output_bytes)
-        print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+        print("Played sound for {:.2f} frequency".format(self.frequency))
 
         stream.stop_stream()
         stream.close()
@@ -45,14 +51,38 @@ class Baudio:
 
 class Transmision:
     def __init__(self):
-        self.t = Baudio(frequency=600.0)
-        self.r = Baudio(frequency=200.0)
+        # 4 baudios
+        # bit/segundo = log_2_(4) = 2
+        self.b1 = Baudio(frequency=1117.0)
+        self.b2 = Baudio(frequency=2117.0)
+        self.b3 = Baudio(frequency=3117.0)
+        self.b4 = Baudio(frequency=4117.0)
 
-    def modular(self, msg):
-        print(f"transmitiendo: [{msg}]")
-        data = [random.choice([0, 1]) for _ in range(10)]
-        for i in data:
-            if i == 0:
-                self.t.transmit()
-            else:
-                self.r.transmit()
+    def modular(self, bits):
+        print(f"transmitiendo: [{bits}]")
+        data = bits
+        # data = [random.choice([0, 1]) for _ in range(10)]
+        for i in range(0, len(data), 2):
+            if data[i] == 0 and data[i+1] == 0:
+                self.b1.transmit()
+            elif data[i] == 0 and data[i+1] == 1:
+                self.b2.transmit()
+            elif data[i] == 1 and data[i+1] == 0:
+                self.b3.transmit()
+            elif data[i] == 1 and data[i+1] == 1:
+                self.b4.transmit()
+
+    def codificar(self, msg, type):
+        # mensaje
+        if type == 0:
+            ba = bitarray.bitarray()
+            ba.frombytes(msg.encode('utf-8'))
+        # archivo
+        if type == 1:
+            ba = []
+            for i in msg:
+                for j in range(7, -1, -1):
+                    bit = (i >> i) & 1
+                    ba.append(bit)
+        print(ba)
+        return ba
