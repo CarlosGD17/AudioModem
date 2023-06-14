d75363a116ba9a8ebabcdae056c4723695161fdc
CarlosGD17 <carlosgd_17@outlook.com>
2023-06-14 15:08:32 -0600

    Recv

--- ModAndDemod/Receptor.py
+++ ModAndDemod/Receptor.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 recibido = []
 data = []
-
+recibiendo = False
 
 def print_sound(indata, outdata, frames, time, status):
     # calcula la longitud del vector
@@ -16,25 +16,20 @@ def print_sound(indata, outdata, frames, time, status):
     print("|" * int(volume_norm))
     if volume_norm > 2:
         data.append(volume_norm)
+        recibiendo = True
+    if recibiendo and volume_norm < 2:
+        stream.stop()
 
-        if 38 < volume_norm < 45:
-            print(f"\n\n---Recibido: 1---\n\n")
-            recibido.append(1)
-            #t.sleep(0.1)
-        if 15 < volume_norm < 20:
-            print(f"\n\n---Recibido: 0---\n\n")
-            recibido.append(0)
-            #t.sleep(0.1)
 
 
 stream = sd.Stream(callback=print_sound, samplerate=44100)
 stream.start()
 
 # Wait for user input to stop the program
-input("Press Enter to stop...\n")
+#input("Press Enter to stop...\n")
 
 # Stop the audio stream and exit the program
-stream.stop()
+#stream.stop()
 
 print(f"\n---Recibido: {recibido}---")
 plt.plot(data)
